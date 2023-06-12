9a0c2ffb4d735857801a72599ce7ffbaf1a64ef8
Bryce Mecum <petridish@gmail.com>
2023-06-11 20:47:46 -0800

    Refactor to use a class

--- main.rb
+++ main.rb
@@ -3,25 +3,50 @@
 require "octokit"
 
 TIME_FMT = "%Y-%m-%d %H:%M:%S %Z"
-SINCE = Time.new(2023, 6, 9, 0, 0, 0, "UTC")
 
-client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
-client.auto_paginate = true
+class Report
+  @client = nil
 
-new_issues = client.list_issues("apache/arrow", {
-    since: SINCE.strftime(TIME_FMT)
-}).filter { |i| i[:created_at] >= SINCE }
+  attr_reader :new_issues
+  attr_reader :new_pull_requests
 
-puts "NEW ISSUES"
-new_issues.each do |issue|
-    puts issue[:title]
+  def initialize
+    @client = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
+    @client.auto_paginate = true
+    set_time_range
+  end
+
+  def set_time_range
+    now = Time.now.utc
+    @since = Time.new(now.year, now.month, now.day - 1, 0, 0, 0, "UTC")
+    @until = Time.new(now.year, now.month, now.day, 0, 0, 0, "UTC")
+  end
+
+  def fetch_data
+    fetch_new_issues
+    fetch_new_pull_requests
+  end
+
+  def fetch_new_issues
+    @new_issues = @client.list_issues("apache/arrow", {
+      since: @since.strftime(TIME_FMT)
+    }).filter { |i| i[:created_at] >= @since && i[:created_at] < @until }
+  end
+
+  def fetch_new_pull_requests
+    @new_pull_requests = @client.pull_requests("apache/arrow", {
+      since: @since.strftime(TIME_FMT)
+    }).filter { |i| i[:created_at] >= @since && i[:created_at] < @until }
+  end
 end
 
-new_pulls = client.pull_requests("apache/arrow", {
-    since: SINCE.strftime(TIME_FMT)
-}).filter { |i| i[:created_at] >= SINCE }
+report = Report.new
+report.fetch_data
 
-puts "NEW_PULLS"
-new_pulls.each do |pull|
-    puts pull[:title]
+report.new_issues.each do |issue|
+  puts issue[:title]
+end
+
+report.new_pull_requests.each do |pull|
+  puts pull[:title]
 end
